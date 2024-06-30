5181cd482424d9bf6246e8d68169e00c5e0e369d
itprojectmanagementbykzn <100278037+itprojectmanagementbykzn@users.noreply.github.com>
2024-06-30 22:26:15 +0700

    Delete mm_to_thai_server-master directory

--- mm_to_thai_server-master/middleware/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(req,res,next){
-    if(req.user.role != 3) return res.status(403).send("Access denied.You are not an admin.");
-
-    next();
-}
--- mm_to_thai_server-master/middleware/cors_options.js
+++
@@ -1,7 +0,0 @@
-const corsOptions = {
-    origin:"*",
-    methods: "GET,POST,PUT,DELETE",
-    credentials:true,
-};
-
-module.exports.corsOptions = corsOptions;
